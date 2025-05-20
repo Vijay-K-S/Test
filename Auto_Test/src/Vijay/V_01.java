2d85d4d4938439e706b964d9743204d76453d96f
Vijay <ksvijaygowda03@gmail.com>
2025-05-20 22:21:20 +0530

    Trail-01

---
+++ Auto_Test/src/Vijay/V_01.java
@@ -0,0 +1,16 @@
+package Vijay;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class V_01 {
+
+	public static void main(String[] args)
+	
+	{
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.co.in/");
+
+	}
+
+}
