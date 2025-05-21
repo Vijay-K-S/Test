36f71095f2c09d5e944a90b0d5b40aa5f99e92a9
Vijay <ksvijaygowda03@gmail.com>
2025-05-21 22:09:52 +0530

    Modified pulled trail

--- Auto_Test/src/Vijay/V_01.java
+++ Auto_Test/src/Vijay/V_01.java
@@ -2,14 +2,14 @@ package Vijay;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class V_01 {
 
 	public static void main(String[] args)
 	
 	{
-		ChromeDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.co.in/");
+		System.out.println("data");
 
 	}
 
